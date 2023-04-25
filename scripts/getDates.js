596153382ef94f4b7e17ac8ac64a77297a00a092
Gavhart <113486288+Gavhart@users.noreply.github.com>
2023-04-25 11:13:40 -0600

    not finished

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,9 +1,6 @@
-let date = new Date();
-document.getElementById('getYear').textContent = date.getFullYear();
+const today = new Date();
+const currentYear = document.querySelector("#currentyear");
+currentYear.textContent = today.getFullYear();
 
-let lastModif = new Date(document.lastModified);
-let day = (lastModif.getMonth() + 1) + "/" + lastModif.getDate() + "/" + lastModif.getFullYear();
-let hour = String(lastModif.getHours());
-let minute = String(lastModif.getMinutes());
-let second = String(lastModif.getSeconds());
-document.getElementById("last-modified").innerHTML = day.padStart(2, '0') + " " + hour.padStart(2, '0') + ':' + minute.padStart(2, '0') + ':' + second.padStart(2, '0');
+const lastupdated = document.querySelector("#lastupdated");
+lastupdated.textContent = document.lastModified;
